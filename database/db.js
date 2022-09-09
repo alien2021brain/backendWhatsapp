cc723e6ef983285e24ac9558860b8c6dd24be79a
Sachin_Pandey <sachinpandey262523@gmail.com>
2022-09-10 03:25:42 +0530

    mgdb

--- database/db.js
+++ database/db.js
@@ -6,7 +6,7 @@ dotenv.config();
 
 
 const Connection = async (username, password) => {
-    const URL = `mongodb+srv://sachin:${password}@cluster0.o4hbbxz.mongodb.net/?retryWrites=true&w=majority`;
+    const URL = `mongodb://${username}:${password}@ac-bry3zbi-shard-00-00.o4hbbxz.mongodb.net:27017,ac-bry3zbi-shard-00-01.o4hbbxz.mongodb.net:27017,ac-bry3zbi-shard-00-02.o4hbbxz.mongodb.net:27017/?ssl=true&replicaSet=atlas-11m9s1-shard-0&authSource=admin&retryWrites=true&w=majority`;
     try {
         await mongoose.connect(URL,{useNewUrlParser: true, useUnifiedTopology: true});
         console.log('Database Connected Succesfully');
