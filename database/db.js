8fc18ad6a646aa57e867bf8eca2c508fd0e515fd
Sachin_Pandey <sachinpandey262523@gmail.com>
2022-09-10 03:51:08 +0530

    mongoose

--- database/db.js
+++ database/db.js
@@ -7,13 +7,17 @@ dotenv.config();
 
 const Connection = async (username, password) => {
     const URL = `mongodb://${username}:${password}@ac-bry3zbi-shard-00-00.o4hbbxz.mongodb.net:27017,ac-bry3zbi-shard-00-01.o4hbbxz.mongodb.net:27017,ac-bry3zbi-shard-00-02.o4hbbxz.mongodb.net:27017/?ssl=true&replicaSet=atlas-11m9s1-shard-0&authSource=admin&retryWrites=true&w=majority`;
-    try {
-        await mongoose.connect(URL,{useNewUrlParser: true, useUnifiedTopology: true});
-        console.log('Database Connected Succesfully');
-    } catch(error) {
-        console.log('Error: ', error.message);
-    }
+    await mongoose
+    .connect(URL, {
+    useUnifiedTopology: true,
+    useNewUrlParser: true,
+    })
+    .then(() => console.log('DB Connected!'))
+    .catch(err => {
+    console.log(DB Connection Error: ${err.message});
+    });
 
 };
+
 
 export default Connection;
